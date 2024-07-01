1184223f6cbbb16f2e5216d1be65aa8a61ddd66c
hikaru1309 <hikarishiro2001@icloud.com>
2024-07-01 16:06:15 +0900

    clear: station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if (check.checked) {
+    console.log("チェックされた！")
+    text.style.backgroundColor = "red"
+  } else {
+    console.log("外れた")
+    text.style.backgroundColor = ""
+  }
 }
