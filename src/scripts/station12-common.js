b55681f468df1974ab8f1a5d092a0a969b093646
hikaru1309 <hikarishiro2001@icloud.com>
2024-07-01 15:47:59 +0900

    clear: station12

--- src/scripts/station12-common.js
+++ src/scripts/station12-common.js
@@ -1,20 +1,6 @@
-// function fetchApi() {
-//   const url = "https://dog.ceo/api/breeds/image/random";
-//   fetch(url)
-//     .then(() => console.log("fetch completed"))
-//     .catch(() => console.error("fetch failed"));
-// }
-
-
-
 function fetchApi() {
   const url = "https://dog.ceo/api/breeds/image/random";
   fetch(url)
-    .then(response => response.json()) // レスポンスをJSONとしてパース
-    .then(data => {
-      const p = document.querySelector('p');
-      p.innerText = JSON.stringify(data); // レスポンスを<p>タグに表示
-      console.log("fetch completed", data);
-    })
-    .catch(error => console.error("fetch failed", error));
+    .then(() => console.log("fetch completed"))
+    .catch(() => console.error("fetch failed"));
 }
